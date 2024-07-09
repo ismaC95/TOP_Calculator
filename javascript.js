3feea547a2672955d583aa100e679b88c67423fe
Ismael <ismacm95@gmail.com>
2024-07-09 22:16:38 +0100

    Adding functionality to display, numbers and operation buttons. Clear button is still a WIP

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-const num1 = 0;
-const num2 = 0;
-const operator = 0;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 let result = 0;
 
 function add(num1, num2) {
@@ -36,5 +36,58 @@ function operate(num1, num2, operator){
     }
     return result;
 }
-operate(2, 3, "x");
+
+function display(){
+    let btnTextArray = [];
+    const btnNumbers = document.querySelectorAll(".number");
+    const display = document.querySelector("#display");
+
+    btnNumbers.forEach(button => {
+        button.addEventListener("click", () => {
+            const btnText = button.textContent;
+            btnTextArray.push(btnText);
+            if(operator === '') {
+                num1 = btnTextArray.join('');
+                display.textContent = num1;
+            }
+            //if (num2 !=== 0), do calculation, display result, num1 = result, num2 = input
+            else{
+                num2 = btnTextArray.join('');
+                display.textContent = num2;
+            }
+    });
+});
+}
+
+const clearBtn = document.querySelector("#clear");
+ clearBtn.addEventListener("click", () => {
+    const display = document.querySelector("#display");
+    display.textContent = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
+    btnTextArray = [];
+    console.log("info is: " + num1, operator, num2);
+ });
+
+function getOperator(){
+    const operatorsBtn = document.querySelectorAll(".operation");
+    operatorsBtn.forEach(button =>{
+        button.addEventListener("click", () => {
+            operator = button.textContent;
+            btnTextArray = [];
+            display();
+            console.log(operator, num1, num2);
+        });
+        
+        
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    display();
+    getOperator();
+});
+
+operate(10, 0.5, "x");
 console.log(result);
